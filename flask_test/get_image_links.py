a02049c5f0d72009cb3ab6ee454d2395e0f5e112
jinmo <yjmd2222@gmail.com>
2023-06-14 19:24:59 +0900

    링크 DB로 업로드로직 작성

--- flask_test/get_image_links.py
+++ flask_test/get_image_links.py
@@ -3,6 +3,7 @@ from flask_test.my_secrets import GOOGLE_CUSTOM_SEARCH_API_KEY, GOOGLE_CUSTOM_SE
 
 def get_image_links(query='correct sitting posture', count=100):
     serachType = 'image'
+    links = []
     for page in range(1, count+1):
         start = (page - 1) * 10 + 1
         url = f'https://www.googleapis.com/customsearch/v1?key={GOOGLE_CUSTOM_SEARCH_API_KEY}&cx={GOOGLE_CUSTOM_SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={serachType}'
@@ -13,10 +14,11 @@ def get_image_links(query='correct sitting posture', count=100):
         for result in results:
             try:
                 print(result['link'])
-                return result['link']
+                links.append(result['link'])
             except:
                 print('no link in result')
-                return None
+
+    return links
             
 if __name__ == '__main__':
     get_image_links()
---
+++ flask_test/upload_links.py
@@ -0,0 +1,41 @@
+import psycopg2
+from flask_test.get_image_links import get_image_links
+from flask_test.my_secrets import DB_HOST_NAME, DB_USER_NAME, DB_PASSWORD, DB_NAME
+
+links = get_image_links()
+
+# connection info
+host = DB_HOST_NAME
+user = DB_USER_NAME
+password = DB_PASSWORD
+database = DB_NAME
+
+# conn
+connection = psycopg2.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
+
+# cur
+cursor = connection.cursor()
+
+# create table
+sql_create_table = f'''
+CREATE TABLE IF NOT EXISTS links (
+    Id SERIAL PRIMARY KEY,
+    Link VARCHAR(1000)
+)
+'''
+cursor.execute(sql_create_table)
+
+sql_insert = f'''
+INSERT INTO links (Link) VALUES %s
+'''
+cursor.executemany(sql_insert, links)
+
+# commit and close
+connection.commit()
+cursor.close()
+connection.close()
