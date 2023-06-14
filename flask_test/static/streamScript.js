9acc4cc09f32c2d694d2da62c67e310781290c53
jinmo <yjmd2222@gmail.com>
2023-06-14 21:32:17 +0900

    fuck chatGPT it's full of shit

--- flask_test/static/streamScript.js
+++ flask_test/static/streamScript.js
@@ -24,6 +24,10 @@ else {
   console.warn('getUserMedia() is not supported by your browser');
 }
 
+function fuckThisShit(wtfYouWant){
+  return wtfYouWant
+}
+
 async function loadAndRunModel() {
   let  movenet = await tf.loadGraphModel(MODEL_PATH, {fromTFHub: true});
   let exampleInputTensor = tf.zeros([1, 192, 192, 3], 'int32');
@@ -33,9 +37,13 @@ async function loadAndRunModel() {
 
   setInterval (async function(){
 
+    
     tf.engine().startScope();
     let imageTensor = tf.browser.fromPixels(video);
-    // let croppedTensor = tf.slice(imageTensor, cropStartPoint, cropSize);
+    let croppedTensor = tf.slice(imageTensor, cropStartPoint, cropSize);
+    // console.log(croppedTensor);
+    let predictions = await predictWebcam();
+    console.log(predictions)
 
     // let resizedTensor = tf.image.resizeBilinear(croppedTensor, [192, 192], true).toInt();
     let resizedTensor = tf.image.resizeBilinear(imageTensor, [192, 192], true).toInt();
@@ -51,7 +59,9 @@ async function loadAndRunModel() {
     // tf.dispose(tensorOutput);
     // tf.dispose(arrayOutput);
     tf.engine().endScope();
-    predictWebcam();
+    // predictWebcam().then(function(prediction) {
+    //   console.log(prediction);
+    // });
 
   }, 1000);
 
@@ -120,47 +130,45 @@ var children = [];
 
 // display
 function predictWebcam() {
-  // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
-    
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
+  return new Promise((resolve) => {
+    // Now let's start classifying a frame in the stream.
+    model.detect(video).then(function (predictions) {
+      // Remove any highlighting we did previous frame.
+      for (let i = 0; i < children.length; i++) {
+        liveView.removeChild(children[i]);
       }
-      // console.log(predictions[n])
+      children.splice(0);
       
-    }
-    
-    // Call this function again to keep predicting when the browser is ready.
-    // window.requestAnimationFrame(predictWebcam);
-    
-    
+      // Now lets loop through predictions and draw them to the live view if
+      // they have a high confidence score.
+      for (let n = 0; n < predictions.length; n++) {
+        // If we are over 66% sure we are sure we classified it right, draw it!
+        if (predictions[n].score > 0.66) {
+          const p = document.createElement('p');
+          p.innerText = predictions[n].class  + ' - with ' 
+              + Math.round(parseFloat(predictions[n].score) * 100) 
+              + '% confidence.';
+          p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+              + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+              + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+          const highlighter = document.createElement('div');
+          highlighter.setAttribute('class', 'highlighter');
+          highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+              + predictions[n].bbox[1] + 'px; width: ' 
+              + predictions[n].bbox[2] + 'px; height: '
+              + predictions[n].bbox[3] + 'px;';
+
+          liveView.appendChild(highlighter);
+          liveView.appendChild(p);
+          children.push(highlighter);
+          children.push(p);
+
+          // Resolve the promise with the current prediction
+          resolve(predictions[n]);
+        }
+      }
+    });
   });
 }
 
