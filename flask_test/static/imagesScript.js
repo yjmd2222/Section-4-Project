2df07a1c2fc0a9c01a04296369e61676f20592c2
jinmo <yjmd2222@gmail.com>
2023-06-15 11:21:55 +0900

    로컬 이미지에 od 로드는 되는데
    위치가 이상하고, 웹 이미지 적용 방법 알아봐야 할 듯

--- flask_test/static/imagesScript.js
+++ flask_test/static/imagesScript.js
@@ -1,8 +1,8 @@
 const MODEL_PATH = 'https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4';
 const preprocessImage = document.getElementById('preprocessImage');
 
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
+// const video = document.getElementById('images');
+const staticView = document.getElementById('staticView');
 const demosSection = document.getElementById('demos');
 const startPreprocessingButton = document.getElementById('startButton');
 
@@ -21,7 +21,7 @@ function getUserMediaSupported() {
 // wants to activate it to call enableCam function which we will 
 // define in the next step.
 if (getUserMediaSupported()) {
-  startPreprocessingButton.addEventListener('click', enableCam)
+  // startPreprocessingButton.addEventListener('click', enableCam)
   startPreprocessingButton.addEventListener('click', loadAndRunModel)
 }
 else {
@@ -39,37 +39,88 @@ let n = 0;
 async function loadAndRunModel() {
   let  movenet = await tf.loadGraphModel(MODEL_PATH, {fromTFHub: true});
   let exampleInputTensor = tf.zeros([1, 192, 192, 3], 'int32');
-  
-  let cropStartPoint = [15, 170, 0];
-  let cropSize = [345, 345, 3];
 
-  let image_index = 0;
 
   setInterval (async function(){
     console.log(links[n]);
     n++;
     
-    var imagePath = "https://t1.daumcdn.net/cfile/tistory/9927E4455A6E154C17";
+    // var imagePath = "https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/standing.jpg";//"https://t1.daumcdn.net/cfile/tistory/9927E4455A6E154C17";
+    var imagePath = "/static/do.png"
+    // var imagePath = links[n];
     preprocessImage.src = imagePath;
     
     tf.engine().startScope();
-    let imageTensor = tf.browser.fromPixels(preprocessImage);
-    // let croppedTensor = tf.slice(imageTensor, cropStartPoint, cropSize);
 
-    // let resizedTensor = tf.image.resizeBilinear(croppedTensor, [192, 192], true).toInt();
-    let resizedTensor = tf.image.resizeBilinear(imageTensor, [192, 192], true).toInt();
+    let predictions = await predictImage();
+    let imageTensor = tf.browser.fromPixels(preprocessImage);
+    const [wHeight, wWidth] = imageTensor.shape;
+
+
+    let bLeft = parseInt(predictions.bbox[0]);
+    let bTop = parseInt(predictions.bbox[1]);
+    let bWidth = parseInt(predictions.bbox[2]);
+    let bHeight = parseInt(predictions.bbox[3]);
+    if (bWidth > wWidth - bLeft) {bWidth = wWidth - bLeft}; // right of the image
+    if (bHeight > wHeight - bTop) {bHeight = wHeight - bTop}; // bottom of the image
+    if (bLeft < 0) {bLeft = 0}; // left of the image
+    if (bTop < 0) {bTop = 0}; // top
+    if (bHeight > bWidth) {bHeight = bWidth}; // if height > width, cut the height from bottom
+    let cropStartPoint = [bTop, bLeft, 0]; // red
+    let cropSize = [bHeight, bWidth, 3] // all RGB
+    // console.log(predictions)
+
+    let padAmount;
+    let padDirection;
+    // console.log('bwidth:'+bWidth + 'bheight:'+bHeight);
+
+    if (bWidth > bHeight) {
+      padAmount = bWidth - bHeight; // most likely not needed
+      padDirection = 'y'
+    }
+    else if (bWidth < bHeight) {
+      padAmount = bHeight - bWidth;
+      padDirection = 'x'
+    }
+
+    // console.log(cropStartPoint);
+    // console.log(cropSize);
+
+    let croppedTensor = tf.slice(imageTensor, cropStartPoint, cropSize);
+    // console.log(croppedTensor.shape);
+
+    let paddedTensor;
+    let resizedTensor;
+    
+    // console.log(padAmount + padDirection);
+
+    if (padDirection = 'x') {
+      paddedTensor = croppedTensor.pad([[0, padAmount], [0, 0], [0, 0]])
+    }
+    else if (padDirection = 'y') {
+      paddedTensor = croppedTensor.pad([[0, 0], [0, padAmount], [0, 0]])
+    }
+    else {
+      paddedTensor = croppedTensor
+    }
+
+    // console.log(paddedTensor.shape);
+    resizedTensor = tf.image.resizeBilinear(paddedTensor, [192, 192], true).toInt();
     // console.log(resizedTensor.shape);
 
     let tensorOutput = movenet.predict(tf.expandDims(resizedTensor));
     let arrayOutput = await tensorOutput.array();
-    console.log(arrayOutput);
+    // console.log(arrayOutput);
     sendPostRequest(arrayOutput);
     // tf.dispose(imageTensor);
     // tf.dispose(croppedTensor);
     // tf.dispose(resizedTensor);
     // tf.dispose(tensorOutput);
-    // tf.dispose(arrayOutput);
+    tf.dispose(arrayOutput);
     tf.engine().endScope();
+    // predictWebcam().then(function(prediction) {
+    //   console.log(prediction);
+    // });
 
   }, 1000);
 
@@ -78,60 +129,107 @@ async function loadAndRunModel() {
 
 
 // Enable the live webcam view and start classification.
-async function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  // if (!model) {
-  //   return;
-  // }
+// async function enableCam(event) {
+//   // Only continue if the COCO-SSD has finished loading.
+//   // if (!model) {
+//   //   return;
+//   // }
   
-  // Hide the button once clicked.
-  event.target.classList.add('removed');  
+//   // Hide the button once clicked.
+//   event.target.classList.add('removed');  
   
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    video: true
-  };
-  let display = await navigator.mediaDevices
-    .getUserMedia(constraints);
+//   // getUsermedia parameters to force video but not audio.
+//   const constraints = {
+//     video: true
+//   };
+//   let display = await navigator.mediaDevices
+//     .getUserMedia(constraints);
 
-  // Returns a sequence of MediaStreamTrack objects 
-  // representing the video tracks in the stream
+//   // Returns a sequence of MediaStreamTrack objects 
+//   // representing the video tracks in the stream
 
-  let settings = display.getVideoTracks()[0]
-      .getSettings();
+//   let settings = display.getVideoTracks()[0]
+//       .getSettings();
 
-  let width = settings.width;
-  let height = settings.height;
+//   let width = settings.width;
+//   let height = settings.height;
 
-  console.log('Actual width of the camera video: '
-      + width + 'px');
-  console.log('Actual height of the camera video:'
-      + height + 'px');
-  const p = document.createElement('p');
-  p.innerText = width + 'X' + height;
-  var d = document.getElementsByTagName('div');
-  d[0].append(p);
+//   console.log('Actual width of the camera video: '
+//       + width + 'px');
+//   console.log('Actual height of the camera video:'
+//       + height + 'px');
+//   const p = document.createElement('p');
+//   p.innerText = width + 'X' + height;
+//   var d = document.getElementsByTagName('div');
+//   d[0].append(p);
 
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
-}
+//   // Activate the webcam stream.
+//   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+//     video.srcObject = stream;
+//     video.addEventListener('loadeddata', predictWebcam);
+//   });
+// }
 
-// Store the resulting model in the global scope of our app.
-// var model = undefined;
+var model = undefined;
 
 // Before we can use COCO-SSD class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment 
 // to get everything needed to run.
 // Note: cocoSsd is an external object loaded from our index.html
 // script tag import so ignore any warning in Glitch.
-// cocoSsd.load().then(function (loadedModel) {
-//   model = loadedModel;
-//   // Show demo section now model is ready to use.
-//   demosSection.classList.remove('invisible');
-// });
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove('invisible');
+});
+
+var children = [];
+
+
+
+// display
+function predictImage() {
+  return new Promise((resolve) => {
+    // Now let's start classifying a frame in the stream.
+    model.detect(preprocessImage).then(function (predictions) {
+      // Remove any highlighting we did previous frame.
+      for (let i = 0; i < children.length; i++) {
+        staticView.removeChild(children[i]);
+      }
+      children.splice(0);
+      
+      // Now lets loop through predictions and draw them to the live view if
+      // they have a high confidence score.
+      for (let n = 0; n < predictions.length; n++) {
+        // If we are over 66% sure we are sure we classified it right, draw it!
+        if (predictions[n].score > 0.66) {
+          const p = document.createElement('p');
+          p.innerText = predictions[n].class  + ' - with ' 
+              + Math.round(parseFloat(predictions[n].score) * 100) 
+              + '% confidence.';
+          p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+              + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+              + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+          const highlighter = document.createElement('div');
+          highlighter.setAttribute('class', 'highlighter');
+          highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+              + predictions[n].bbox[1] + 'px; width: ' 
+              + predictions[n].bbox[2] + 'px; height: '
+              + predictions[n].bbox[3] + 'px;';
+
+          staticView.appendChild(highlighter);
+          staticView.appendChild(p);
+          children.push(highlighter);
+          children.push(p);
+
+          // Resolve the promise with the current prediction
+          resolve(predictions[n]);
+        }
+      }
+    });
+  });
+}
 
 
 // 주기적으로 POST 요청을 보내는 함수
@@ -141,8 +239,8 @@ async function sendPostRequest(output) {
       movenet_output: output
   };
   // POST 요청을 보냅니다.
-  fetch('/record-images', {
-      method: 'GET',
+  fetch('/post-endpoint', {
+      method: 'POST',
       headers: {
           'Content-Type': 'application/json'
       },
@@ -155,48 +253,3 @@ async function sendPostRequest(output) {
 
 // 특정 간격(밀리초)마다 POST 요청을 보내도록 설정합니다.
 // setInterval(sendPostRequest, 5000); // 5초마다 POST 요청을 보냅니다.
-
-// 이미지 링크를 가져오는 함수
-
-const API_KEY = 'replace';
-const SEARCH_ENGINE_ID = 'replace';
-
-
-// function fetchImageLinks() {
-//   // Google Custom Search API를 통해 이미지 링크를 가져오는 AJAX 요청 등을 수행합니다.
-//   // 이 예시에서는 fetch() 함수를 사용하도록 가정합니다.
-//   var url = `https://www.googleapis.com/customsearch/v1?cx=${SEARCH_ENGINE_ID}&key=${API_KEY}&q=correct%20sitting%20posture&searchType=image`
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       // 이미지 링크가 포함된 결과 데이터를 받아옵니다.
-//       const imageLinks = data.items.map(item => item.link);
-      
-//       // 이미지 링크를 웹 페이지에 표시합니다.
-//       displayImages(imageLinks);
-//     })
-//     .catch(error => {
-//       console.error('Error fetching image links:', error);
-//     });
-// }
-
-// // 이미지를 표시하는 함수
-// function displayImages(imageLinks) {
-//   const imageContainer = document.getElementById('imageContainer');
-  
-//   // 기존에 표시된 이미지를 제거합니다.
-//   imageContainer.innerHTML = '';
-  
-//   // 이미지를 생성하고 컨테이너에 추가합니다.
-//   imageLinks.forEach(link => {
-//     const image = document.createElement('img');
-//     image.src = link;
-//     imageContainer.appendChild(image);
-//   });
-// }
-
-// // 페이지 로드 후 5초마다 이미지 링크를 가져오기 위해 setInterval() 함수를 사용합니다.
-// window.addEventListener('load', () => {
-//   fetchImageLinks(); // 페이지 로드 후 첫 번째 이미지 링크를 가져옵니다.
-//   setInterval(fetchImageLinks, 5000); // 5초마다 이미지 링크를 업데이트합니다.
-// });
