6b8d9c192c54b73d5e2657310c634f7cc53f2ac9
jinmo <yjmd2222@gmail.com>
2023-06-15 23:14:36 +0900

    이미지 데이터 확보 준비만 몇 번재인지 모르겠음

--- send_data.py
+++ send_data.py
@@ -1,6 +1,8 @@
 from flask_test.get_links_and_upload import get_image_links, send_links_to_db
 
-posture = 'forward head'
+postures = ['normal', 'forward_head', 'leaning']
+search_terms = ['correct sitting posture', 'forward head posture', 'leaning back in chair']
 
-links = get_image_links()
-send_links_to_db(links, posture)
+for idx, posture in enumerate(postures):
+    links = get_image_links(query=search_terms[idx])
+    send_links_to_db(links, posture)
